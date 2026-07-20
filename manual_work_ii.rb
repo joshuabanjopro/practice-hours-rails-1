d2f10332fa9c4bb8a43964561cb6a565335d724c
Joshua Banjo <joshuabanjopro@gmail.com>
2026-07-20 20:17:52 +0100

    I have always liked working with loops

---
+++ manual_work_ii.rb
@@ -0,0 +1,7 @@
+nba_starting_five = ['Stephen Curry', 'Michael Jordan', 'LeBron James', 'Giannis', 'Shaq']
+int_counter = 1
+
+for nba_player in nba_starting_five
+  puts "And in the #{int_counter} spot, we have #{nba_player}"
+  int_counter += 1
+end
